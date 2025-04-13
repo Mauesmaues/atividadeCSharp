1a4fc2ce5333b38f366102e535c42195bc0a81e1
Marcos Maues <yboostx@gmail.com>
2025-04-13 18:47:33 -0300

    "att"

--- ex2/programEx2/Program.cs
+++ ex2/programEx2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Por favor, digite seu ano de nascimento:");
+        
+        try
+        {
+            int anoNascimento = int.Parse(Console.ReadLine());
+            int anoAtual = 2024;
+            int idade = anoAtual - anoNascimento;
+            
+            Console.WriteLine($"Sua idade em 2024 é: {idade} anos.");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Entrada inválida. Por favor, digite um número válido.");
+        }
+        
+        Console.WriteLine("Pressione qualquer tecla para sair...");
+        Console.ReadKey();
+    }
+}
--- ex5/programEx5/Program.cs
+++ ex5/programEx5/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Digite um número para verificar se é par ou ímpar:");
+            int numero = int.Parse(Console.ReadLine());
+            
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine($"O número {numero} é par.");
+            }
+            else
+            {
+                Console.WriteLine($"O número {numero} é ímpar.");
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Entrada inválida. Por favor, digite um número válido.");
+        }
+        
+        Console.WriteLine("Pressione qualquer tecla para sair...");
+        Console.ReadKey();
+    }
+}
